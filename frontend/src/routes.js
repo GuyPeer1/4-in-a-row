d234f705091fca0658e6472d8c36f456721c4af6
ofek ashkenazi <ofeka18@gmail.com>
2023-03-05 15:57:32 +0200

    worked on two modals rules and home, added fonts and ending this two modals

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,15 +1,19 @@
 import { BoardPage } from './pages/board-page.jsx'
 import { HomePage } from './pages/home-page.jsx'
+import { RulesPage } from './pages/rules-page.jsx'
 
 const routes = [
     {
         path: '/',
         component: <HomePage />
     },
-
     {
         path: '/board-page',
         component: <BoardPage />
+    },
+    {
+        path: '/rules',
+        component: <RulesPage />
     }
 ]
 

1a56ac5bacfdf563528619a3785f92f2f7ef23d2
Guy Peer <peer.g2@gmail.com>
2023-03-05 13:39:05 +0200

    fixed img bacgfround for board

--- frontend/src/pages/home-page.jsx
+++ frontend/src/pages/home-page.jsx
@@ -4,19 +4,20 @@ import { boardService } from '../services/board.service'
 export function HomePage() {
 
     const [board, setBoard] = useState(boardService.getEmptyBoard())
-
-    console.log(board)
+    console.table(board)
     return (
         <section className='home-page'>
             <h2>hiii</h2>
             <table>
-                <tbody>
-                   {board.map((row,index) => <tr key={index}>
-                    {board.map((cell,indexC) => <td key={indexC} className="cell"></td> )}
-                   </tr> )}
-                </tbody>
-            </table>
+                    {board.map((row, index) => <tr key={index}>
+                        
+                        {row.map((cell, indexC) => <td key={indexC} >
+                            <div className="cell"></div>
+                        </td>
+                        )}
 
+                    </tr>)}
+            </table>
         </section >
     )
 }

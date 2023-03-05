ffa88092cd2eabc49cb6a108b15872a21526a60e
Guy Peer <peer.g2@gmail.com>
2023-03-05 10:38:02 +0200

    project inital tamplet

---
+++ frontend/src/pages/home-page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function HomePage() {
+    return (
+        <section>
+           <h2>hello</h2>
+        </section >
+    )
+}
---
+++ frontend/src/root-cmp.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+
+import routes from './routes'
+
+import { AppHeader } from './cmps/app-header'
+import { UserMsg } from './cmps/user-msg'
+
+export function RootCmp() {
+
+    return (
+        <section className="main-layout">
+            <AppHeader />
+            <main>
+                <Routes>
+                    {routes.map(route => <Route key={route.path} exact={true} element={route.component} path={route.path} />)}
+                </Routes>
+            </main>
+            <UserMsg />
+        </section>
+    )
+}
+
+

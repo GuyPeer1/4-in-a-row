be8acb849192617144064fbaf802b6ff52e5575d
Guy Peer <peer.g2@gmail.com>
2023-03-05 17:33:39 +0200

    fixed img-text inside turn cmp

--- frontend/src/cmps/turn.jsx
+++ frontend/src/cmps/turn.jsx
@@ -2,12 +2,10 @@ import marker from '../assets/imgs/turn-background-red.svg'
 
 export default function Turn({ name = "PLAYER 1" }) {
     return <section className="turn">
-        {/* <div className="img-container"> */}
         <img className="img-env" src={marker} />
         <div className="img-txt">
             <span className='player-name'>{name}'S TURN</span>
-            <span className='time-left'>30s</span>
+            <span className='time-left'>3s</span>
         </div>
-        {/* </div> */}
     </section>
 }
